5f64215424c4a3a4a4a501f3f48826ac8efcc07d
Bill Boughton <boughton.william@gmail.com>
2017-06-05 08:44:15 -0600

    initial commit

---
+++ Emoji_Dictionary/Emoji_Dictionary/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Emoji_Dictionary
+//
+//  Created by Bill Boughton on 6/4/17.
+//  Copyright © 2017 Bill Boughton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    
+    var emojis = ["😎", "😡", "💩", "🙏", "✌️", "👀", "🦂"]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableview.dataSource = self
+        tableview.delegate = self
+        
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
