7e6bbee8e508e2bcf5cff2dbe7e74debe66c4baa
Bill Boughton <boughton.william@gmail.com>
2017-06-06 16:17:47 -0600

    first attempt at bring data into custom view

--- customTableViewsExample/customTableViewsExample/ViewController.swift
+++ customTableViewsExample/customTableViewsExample/ViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     let animals = ["panda", "lion", "elefant"]
+    var everyThing = [AnyObject]()
+
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return animals.count
@@ -26,6 +29,39 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("https://pantrysupply.herokuapp.com/getall").responseJSON { response in
+            //            print(response.request!)  // original URL request
+            //            print(response.response!) // HTTP URL response
+            //            print(response.data!)     // server data
+            //            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value as AnyObject?{
+                //                print("JSON: \(JSON)")
+                if let nextArray = JSON["result"] as! NSArray?{
+                    for i in 0..<nextArray.count{
+                        var someDict = [String: AnyObject]()
+                        print(i)
+                        //                        print(nextArray.count)
+                        let nextTempHolder = nextArray[i] as AnyObject?
+                        //                        print(nextTempHolder!)
+                        //                        let finalHolder = String(describing: nextTempHolder?["quantity"]) as String
+                        let finalHolder = nextTempHolder?["quantity"] as! Int
+                        let idHolder = nextTempHolder?["item_id"] as! String?
+                        let brandNameHolder = nextTempHolder?["brand_name"] as! String?
+                        let itemName = nextTempHolder?["item_name"] as! String?
+                        someDict["brand_item"] = brandNameHolder! as AnyObject
+                        someDict["item_name"] = itemName as AnyObject
+                        someDict["item_id"] = idHolder as AnyObject
+                        someDict["quantity"] = finalHolder as AnyObject
+                        self.everyThing.append(someDict as AnyObject)
+                    }
+                    print(self.everyThing[0]["brand_item"]!!)
+                }
+            }
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
